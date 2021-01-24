672f0c0e871878e5815de961c952bab5e696e912
piyushthummar <piyushthummar305@gmail.com>
2021-01-24 10:22:46 -0500

    Question-2 passed all tests

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -36,6 +36,8 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for key,value in orderList:
+        totalCost = totalCost + value * fruitPrices[key]
     return totalCost
 
 
